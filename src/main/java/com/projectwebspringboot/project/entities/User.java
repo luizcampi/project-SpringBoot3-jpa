ee4b243c958eeec2973b4c483471cc0ed0edc6ef
Luiz Carlos <luizcampi11@gmail.com>
2023-01-23 18:09:00 -0300

    Exception findById, delete, update

--- src/main/java/com/projectwebspringboot/project/entities/User.java
+++ src/main/java/com/projectwebspringboot/project/entities/User.java
@@ -16,9 +16,9 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,11 +26,11 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	@JsonIgnore
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
-	
+
 	public User() {
 	}
 
@@ -82,7 +82,7 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public List<Order> getOrders() {
 		return orders;
 	}
@@ -103,8 +103,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
-	
 }
